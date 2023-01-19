9c9052403c182fbdaa91d871ac17c79f2a8d3ef5
Xabier Matute Rusinol <xmatute-@c3r8s5.42urduliz.com>
2023-01-19 14:44:34 +0100

    version 2  de validsintax

--- Parse/validsintax.c
+++ Parse/validsintax.c
@@ -6,33 +6,36 @@
 /*   By: xmatute- <xmatute-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/19 14:28:44 by xmatute-          #+#    #+#             */
-/*   Updated: 2023/01/19 14:37:11 by xmatute-         ###   ########.fr       */
+/*   Updated: 2023/01/19 14:44:14 by xmatute-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include ../minishell.h
 
+static int	checkclosed(char *s, char c)
+{
+	if (!closed(s, c))
+	{
+		error_closed(c);
+		return (0);
+	}
+	return (1);
+}
+
+static int	checkcontain(char *s, char c)
+{
+	if (!contain(s, c))
+	{
+		error_contain(c);
+		return (0);
+	}
+	return (1);
+}
+
 int validsintax(char *s)
 {
-	if (!closed(s, '"'))
-	{
-		error_closed('"');
-		return (0);
-	}
-	if (!closed(s, 27))
-	{
-		error_closed(27);
-		return (0);
-	}
-	if (contain(s, 92))
-	{
-		error_contain(92);
-		return (0);
-	}
-	if (contain(s, ';'))
-	{
-		error_contain(';');
-		return (0);
-	}	
-	return (1);
+	return (checkclosed(s, '"') &&
+			checkclosed(s, ''') &&
+			checkcontain(s, '\') &&
+			checkcontain(s, ';'));
 }
