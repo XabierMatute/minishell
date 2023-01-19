74ca5257ab7ed3969c330ede6609bb4a821ac1f6
Xabier Matute Rusinol <xmatute-@c3r8s5.42urduliz.com>
2023-01-19 14:38:30 +0100

    primera version de validsintax

--- Parse/validsintax.c
+++ Parse/validsintax.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   validsintax.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: xmatute- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: xmatute- <xmatute-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/19 14:28:44 by xmatute-          #+#    #+#             */
-/*   Updated: 2023/01/19 14:28:44 by xmatute-         ###   ########.fr       */
+/*   Updated: 2023/01/19 14:37:11 by xmatute-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,25 @@
 
 int validsintax(char *s)
 {
-	
+	if (!closed(s, '"'))
+	{
+		error_closed('"');
+		return (0);
+	}
+	if (!closed(s, 27))
+	{
+		error_closed(27);
+		return (0);
+	}
+	if (contain(s, 92))
+	{
+		error_contain(92);
+		return (0);
+	}
+	if (contain(s, ';'))
+	{
+		error_contain(';');
+		return (0);
+	}	
+	return (1);
 }
