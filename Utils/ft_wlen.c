9145c644b6877c02e9e01e280c28a43a5c2a5136
Xabier Matute Rusinol <xmatute-@c3r3s4.42urduliz.com>
2023-01-29 18:11:07 +0100

    version buena del split

--- Utils/ft_wlen.c
+++ Utils/ft_wlen.c
@@ -6,7 +6,7 @@
 /*   By: xmatute- <xmatute-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/29 16:56:29 by xmatute-          #+#    #+#             */
-/*   Updated: 2023/01/29 17:01:39 by xmatute-         ###   ########.fr       */
+/*   Updated: 2023/01/29 17:40:10 by xmatute-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,9 @@ size_t	ft_wlen(const char *s, char c)
 
 	l = 0;
 	while (s[l] && s[l] != c)
+	{
+		l = ignoreq(&s[l]) - s;
 		l++;
+	}
 	return (l);
 }
