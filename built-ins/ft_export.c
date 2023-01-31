21350d43bec37e240f29b9aaf99d7b1abbabeddf
Jon Perez <jperez@c3r5s5.42urduliz.com>
2023-01-31 20:13:46 +0100

    finished export

--- built-ins/ft_export.c
+++ built-ins/ft_export.c
@@ -6,7 +6,7 @@
 /*   By: jperez <jperez@student.42urduliz.>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/29 19:09:40 by jperez            #+#    #+#             */
-/*   Updated: 2023/01/31 19:43:46 by jperez           ###   ########.fr       */
+/*   Updated: 2023/01/31 20:12:53 by jperez           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,6 @@ int ft_check_variable(char *variable)
 	int	equal_pos;
 
 	equal_pos = ft_strchr(variable, '=') - variable;
-	printf("%d\n", equal_pos);
 	if (equal_pos <= 0)
 		return (1);
 	return (0);
@@ -39,6 +38,10 @@ void	ft_export_error(char *variable)
 	int equal_pos;
 
 	equal_pos = ft_strchr(variable, '=') - variable;
+	if (equal_pos == 0)
+		ft_printf("export: '%s': not a variable identifier\n", variable);
+	else if (equal_pos < 0)
+		ft_printf("export: intruduce a '=' to export a variable\n");
 	
 }
 
@@ -52,6 +55,6 @@ void	ft_export(char **env, char **variables)
 		if (!ft_check_variable(variables[i]))
 			ft_add_variable(env, variables[i]);
 		else
-			ft_printf("Error\n");
+			ft_export_error(variables[i]);
 	}
 }
