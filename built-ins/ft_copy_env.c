b83994319019e538104d69b6348ab2fdfb0b6c71
Jon Perez <jperez@c3r3s2.42urduliz.com>
2023-02-08 17:19:47 +0100

    created stack in ft_copy_env.c

--- built-ins/ft_copy_env.c
+++ built-ins/ft_copy_env.c
@@ -6,7 +6,7 @@
 /*   By: jperez <jperez@student.42urduliz.>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/01 17:23:49 by jperez            #+#    #+#             */
-/*   Updated: 2023/02/07 17:32:27 by jperez           ###   ########.fr       */
+/*   Updated: 2023/02/07 20:54:03 by jperez           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,12 @@ t_stack	*ft_create_env(char **env)
 {
 	int		i;
 
-	G_cp_env = (t_stack *)malloc(sizeof(t_stack));
+	G_cp_env = ft_create_stack(); 
 	if (!env)
 		return (NULL);
 	i = -1;
 	while (env[++i])
 		ft_push(G_cp_env, ft_strdup(env[i]));
 	return (G_cp_env);
+	printf("HOLA\n");
 }
