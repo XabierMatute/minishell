6b86a6e05369569032003f51ff5419bb125d88ce
Jon Perez <jperez@c4r4s4.42urduliz.com>
2023-02-16 16:14:51 +0100

    mergeee

--- built-ins/ft_manage_builtins.c
+++ built-ins/ft_manage_builtins.c
@@ -6,7 +6,7 @@
 /*   By: xmatute- <xmatute-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/10 16:33:20 by jperez            #+#    #+#             */
-/*   Updated: 2023/02/13 15:09:20 by xmatute-         ###   ########.fr       */
+/*   Updated: 2023/02/15 18:06:08 by jperez           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,32 +14,32 @@
 
 int	ft_manage_builtins(char **comands)//apañar lo de como le llega la entrada
 {
-	if (!strncmp(comands[0], "echo", ft_strlen(comands[0])))//probar a poner echooooo
+	if (!ft_strncmp(comands[0], "echo", ft_strlen(comands[0])))//probar a poner echooooo
 	{
 		ft_echo(comands + 1);
 		return (1);
 	}
-	else if (!strncmp(comands[0], "cd", ft_strlen(comands[0])))
+	else if (!ft_strncmp(comands[0], "cd", ft_strlen(comands[0])))
 	{
 		ft_cd(comands + 1);
 		return (1);
 	}
-	else if (!strncmp(comands[0], "pwd", ft_strlen(comands[0])))
+	else if (!ft_strncmp(comands[0], "pwd", ft_strlen(comands[0])))
 	{
 		ft_pwd(comands + 1);
 		return (1);
 	}
-	else if (!strncmp(comands[0], "export", ft_strlen(comands[0])))
+	else if (!ft_strncmp(comands[0], "export", ft_strlen(comands[0])))
 	{
 		ft_export(comands + 1);
 		return (1);
 	}
-	else if (!strncmp(comands[0], "unset", ft_strlen(comands[0])))
+	else if (!ft_strncmp(comands[0], "unset", ft_strlen(comands[0])))
 	{
 		ft_unset(comands + 1);
 		return (1);
 	}
-	else if (!strncmp(comands[0], "env", ft_strlen(comands[0])))
+	else if (!ft_strncmp(comands[0], "env", ft_strlen(comands[0])))
 	{
 		ft_env(comands + 1);
 		return (1);//poner todos enel return para aorra lineas
