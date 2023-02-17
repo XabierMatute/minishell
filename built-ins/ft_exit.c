ec61fa27a804bf5fe3b35dfc2b182b54320f978d
Jon Perez <jperez@c4r4s3.42urduliz.com>
2023-02-17 18:00:00 +0100

    protected ft_exit and changed return

--- built-ins/ft_exit.c
+++ built-ins/ft_exit.c
@@ -6,7 +6,7 @@
 /*   By: xmatute- <xmatute-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/01 19:09:12 by jperez            #+#    #+#             */
-/*   Updated: 2023/02/16 15:01:30 by xmatute-         ###   ########.fr       */
+/*   Updated: 2023/02/17 17:59:31 by jperez           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,13 @@
 
 int	ft_exit(char **args)
 {
+	if (ft_args_lenght(args) > 1)
+	{
+		ft_printf("❌ Too many arguments\n");
+		return (1);
+	}
 	ft_printf("exit\n");
 	destroy_stack(G_cp_env);
-	exit(0);
+	exit(ft_atoi(*args));
 	return (97);
 }
