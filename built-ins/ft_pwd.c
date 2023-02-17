5cd8ab1032bbb5fd722b23e0604dfad07c9fc7a5
Jon Perez <jperez@c4r4s3.42urduliz.com>
2023-02-17 17:43:51 +0100

    changed ft_pwd.c raturn and protec the functin

--- built-ins/ft_pwd.c
+++ built-ins/ft_pwd.c
@@ -6,7 +6,7 @@
 /*   By: xmatute- <xmatute-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/29 16:15:35 by jperez            #+#    #+#             */
-/*   Updated: 2023/02/16 14:51:47 by xmatute-         ###   ########.fr       */
+/*   Updated: 2023/02/17 17:42:56 by jperez           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,9 +27,10 @@ int	ft_pwd(char **args)
 	char	buff[PATH_MAX];
 
 	if (ft_check_args(args))
-		return (55);
+		return (1);
 	getcwd(buff, PATH_MAX);
+	if (!buff)
+		return (1);
 	ft_printf("%s\n", buff);
-	return (55);
-
+	return (0);
 }
