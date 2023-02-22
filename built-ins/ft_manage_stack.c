ecbc65e7ed33db754ce6252a4a42b4cf2831e958
Xabier Matute Rusinol <xmatute-@c4r6s3.42urduliz.com>
2023-02-22 15:11:23 +0100

    cooregido un fallito en manage stack,

--- built-ins/ft_manage_stack.c
+++ built-ins/ft_manage_stack.c
@@ -6,7 +6,7 @@
 /*   By: xmatute- <xmatute-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/01 17:17:26 by jperez            #+#    #+#             */
-/*   Updated: 2023/02/10 18:10:59 by xmatute-         ###   ########.fr       */
+/*   Updated: 2023/02/22 12:26:42 by xmatute-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@ void	destroy_stack(t_stack *stack)
 	while (stack->peek != NULL)
 		ft_pop(stack, 0);
 	free(stack);
-	stack = NULL;
+	G_cp_env = NULL;// o asi o cambiar el imput a **stack
 }
 
 t_node	*ft_create_node(char *_variable)
