019a143c8133721edfa20d92e8bfa731484cf85a
Jon Perez <jperez@c4r6s3.42urduliz.com>
2023-02-24 18:20:18 +0100

    punto error

--- Utils/ft_execve.c
+++ Utils/ft_execve.c
@@ -6,13 +6,13 @@
 /*   By: jperez <jperez@student.42urduliz.>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/24 16:48:02 by jperez            #+#    #+#             */
-/*   Updated: 2023/02/24 17:42:52 by jperez           ###   ########.fr       */
+/*   Updated: 2023/02/24 18:19:51 by jperez           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-void	ft_execve_errors(char *path)
+int	ft_execve_errors(char *path)
 {
 	if (contain(path, '/'))
 	{
@@ -22,9 +22,10 @@ void	ft_execve_errors(char *path)
 			ft_printf("%s: No such file or directory\n", path);
 	}
 	else if (!ft_strncmp(path, ".\0", 2))
-		ft_printf("%s: filename argument required\n", path);
+		return (ft_printf("%s: filename argument required\n", path), 2);	
 	else
 		ft_printf("%s: command not found\n", path);
+	return (127);
 }
 
 int	ft_execve(char *path, char **comands)
@@ -36,11 +37,11 @@ int	ft_execve(char *path, char **comands)
 	res = execve(path, comands, env);
 	if (res  == -1)//hacer esto mejor
 	{
-		ft_execve_errors(path);
+		res = ft_execve_errors(path);
 		ft_free_2d_arr((void **)comands);
 		destroy_stack(G_cp_env);
 		ft_free_2d_arr((void **)env);
-		return (127);
+		return (res);
 	}
 	ft_free_2d_arr((void **)env);
 	return (0);
