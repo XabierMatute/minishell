decc52056c4335f1ed782d7689db04bcfccdd03b
Jon Perez <jperez@c4r5s4.42urduliz.com>
2023-02-28 17:02:26 +0100

    normi ft_pwd

--- built-ins/ft_pwd.c
+++ built-ins/ft_pwd.c
@@ -6,7 +6,7 @@
 /*   By: xmatute- <xmatute-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/29 16:15:35 by jperez            #+#    #+#             */
-/*   Updated: 2023/02/23 17:43:32 by jperez           ###   ########.fr       */
+/*   Updated: 2023/02/28 17:01:55 by jperez           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,13 @@ static int	ft_check_args(char **args)
 {
 	if (ft_args_lenght(args) > 0)
 	{
-
 		ft_printf("❌ env: to many arguments\n");
 		return (1);
 	}
 	return (0);
 }
 
-char *ft_getpwd()
+char	*ft_getpwd(void)
 {
 	char	*pwd;
 
