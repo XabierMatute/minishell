f75ad8d9a6e854fdfabc047f13738b9e15dd27f8
Xabier Matute Rusinol <xmatute-@c4r8s2.42urduliz.com>
2023-03-02 12:44:00 +0100

    Utils/ignoreq.c normalizado

--- Utils/ignoreq.c
+++ Utils/ignoreq.c
@@ -6,24 +6,24 @@
 /*   By: xmatute- <xmatute-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/19 18:23:36 by xmatute-          #+#    #+#             */
-/*   Updated: 2023/01/29 16:35:20 by xmatute-         ###   ########.fr       */
+/*   Updated: 2023/03/02 12:05:05 by xmatute-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-char *ignore(char *s, char c)
+char	*ignore(char *s, char c)
 {
-		if (*s == c)
-		{
+	if (*s == c)
+	{
+		s++;
+		while (*s != c && *s)
 			s++;
-			while (*s != c && *s)
-				s++;
-		}
-		return (s);
+	}
+	return (s);
 }
 
-char *ignoreq(char *s)
+char	*ignoreq(char *s)
 {
 	s = ignore(s, '"');
 	s = ignore(s, 39);
