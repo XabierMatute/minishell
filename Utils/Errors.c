13c0ad7057cb9e55321bcb119c615371472e6c0d
Jon Perez <jperez@c4r5s2.42urduliz.com>
2023-03-05 20:16:44 +0100

    Errors.c normi

--- Utils/Errors.c
+++ Utils/Errors.c
@@ -6,27 +6,27 @@
 /*   By: xmatute- <xmatute-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/11 21:38:50 by xmatute-          #+#    #+#             */
-/*   Updated: 2023/02/13 20:44:45 by xmatute-         ###   ########.fr       */
+/*   Updated: 2023/03/05 20:16:28 by jperez           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-int merror(void)
+int	merror(void)
 {
 	ft_printf("⚠️Error de memoria\n");
 	destroy_stack(G_cp_env);
 	return (1);
 }
 
-int rerror(void)
+int	rerror(void)
 {
 	ft_printf("⚠️Error en las redirecciones\n");
 	destroy_stack(G_cp_env);
 	return (1);
 }
 
-int serror(void)
+int	serror(void)
 {
 	ft_printf("⚠️Error en las señales\n");
 	destroy_stack(G_cp_env);
