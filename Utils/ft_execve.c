58e685a180c839f1c1de4c3793844cdc985c4c17
Jon Perez <jperez@c4r5s2.42urduliz.com>
2023-03-05 20:08:59 +0100

    normi ft_execve

--- Utils/ft_execve.c
+++ Utils/ft_execve.c
@@ -6,7 +6,7 @@
 /*   By: jperez <jperez@student.42urduliz.>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/24 16:48:02 by jperez            #+#    #+#             */
-/*   Updated: 2023/02/24 18:19:51 by jperez           ###   ########.fr       */
+/*   Updated: 2023/03/05 20:08:13 by jperez           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ int	ft_execve_errors(char *path)
 			ft_printf("%s: No such file or directory\n", path);
 	}
 	else if (!ft_strncmp(path, ".\0", 2))
-		return (ft_printf("%s: filename argument required\n", path), 2);	
+		return (ft_printf("%s: filename argument required\n", path), 2);
 	else
 		ft_printf("%s: command not found\n", path);
 	return (127);
@@ -35,7 +35,7 @@ int	ft_execve(char *path, char **comands)
 
 	env = ft_transform_env();
 	res = execve(path, comands, env);
-	if (res  == -1)//hacer esto mejor
+	if (res == -1)
 	{
 		res = ft_execve_errors(path);
 		ft_free_2d_arr((void **)comands);
