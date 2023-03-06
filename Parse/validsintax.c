fd9a71ddb288b18876b1edb8b7c065aff74507bb
Xabier Matute Rusinol <xmatute-@c4r8s5.42urduliz.com>
2023-03-06 16:51:03 +0100

    validsintax.c normalizado

--- Parse/validsintax.c
+++ Parse/validsintax.c
@@ -6,32 +6,15 @@
 /*   By: xmatute- <xmatute-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/19 14:28:44 by xmatute-          #+#    #+#             */
-/*   Updated: 2023/02/13 20:39:56 by xmatute-         ###   ########.fr       */
+/*   Updated: 2023/03/06 16:49:34 by xmatute-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-
-
-int validsintax(char *s)
+int	validsintax(char *s)
 {
-	return (checknoin(s) &&
-			checkpipes(s) &&
-			checkdiples(s));
+	return (checknoin(s)
+		&& checkpipes(s)
+		&& checkdiples(s));
 }
-
-
-/*
-** no entender || y && y que haya varios ||| contrabarras en cualquier sitio | al principio
-*/
-
-
-/*
-** 39  '
-** 92  \
-*/
-
-/*
-** expandir las variables y eliminr las comillas lo que sea que es $?
-*/
