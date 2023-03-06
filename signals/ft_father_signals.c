2ab0d2814bfdbfc2158d3e43c99ea4e7e4cb427a
Jon Perez <jperez@c4r7s4.42urduliz.com>
2023-03-06 17:27:24 +0100

    normi ftfather_signals.c

--- signals/ft_father_signals.c
+++ signals/ft_father_signals.c
@@ -6,39 +6,40 @@
 /*   By: jperez <jperez@student.42urduliz.>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/08 18:30:39 by jperez            #+#    #+#             */
-/*   Updated: 2023/03/04 19:11:50 by jperez           ###   ########.fr       */
+/*   Updated: 2023/03/06 17:26:57 by jperez           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-static void	ft_manage_terminal() {
-  struct termios tty;
-  tcgetattr(STDIN_FILENO, &tty);
-  tty.c_lflag &= ~ECHOCTL;
-  tcsetattr(STDIN_FILENO, TCSANOW, &tty);
+static void	ft_manage_terminal(void)
+{
+	struct termios	tty;
+
+	tcgetattr(STDIN_FILENO, &tty);
+	tty.c_lflag &= ~ECHOCTL;
+	tcsetattr(STDIN_FILENO, TCSANOW, &tty);
 }
 
 void	ft_sigint_action(void)
 {
-		rl_on_new_line();
-		write(1, "\n", 1);
-		rl_replace_line("", 1);
-   		rl_redisplay();
+	rl_on_new_line();
+	write(1, "\n", 1);
+	rl_replace_line("", 1);
+	rl_redisplay();
 }
 
-static	void ft_manage_signals(int signum)
+static void	ft_manage_signals(int signum)
 {
 	if (signum == SIGQUIT)
 		rl_redisplay();
-
 	else if (signum == SIGINT)
 		ft_sigint_action();
 }
 
-int ft_add_father_listener()
+int	ft_add_father_listener(void)
 {
-	type_sa sa;
+	type_sa	sa;
 
 	sa.sa_sigaction = 0;
 	sa.sa_mask = 0;
@@ -47,7 +48,5 @@ int ft_add_father_listener()
 	ft_manage_terminal();
 	if (ft_siagction(SIGQUIT, &sa) || ft_siagction(SIGINT, &sa))
 		return (1);
-
 	return (0);
 }
-
