f2a80384569f05492094dbe196d3c7f9bb975edc
Jon Perez <jperez@c4r7s4.42urduliz.com>
2023-03-06 17:22:10 +0100

    pipes.c

--- Pipes/pipes.c
+++ Pipes/pipes.c
@@ -6,13 +6,13 @@
 /*   By: xmatute- <xmatute-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/29 19:04:38 by xmatute-          #+#    #+#             */
-/*   Updated: 2023/02/18 18:35:42 by xmatute-         ###   ########.fr       */
+/*   Updated: 2023/03/06 17:21:27 by jperez           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-int pipes(char **comands)
+int	pipes(char **comands)
 {
 	int		i;
 	int		**pipes;
@@ -23,22 +23,17 @@ int pipes(char **comands)
 	pipes = ft_create_pipes(ft_args_lenght(comands) - 1);
 	if (ft_args_lenght(comands) > 1 && !pipes)
 		merror();
-					// printf("----->Father: %d\n", getpid());
-	i = 0;
-	while (comands[i] && G_cp_env)
-	{
-		ft_family_process(pipes, comands[i], i);	
-		i++;//ponlo en una linea si quieres
-	}
+	i = -1;
+	while (comands[++i] && G_cp_env)
+		ft_family_process(pipes, comands[i], i);
 	if (pipes)
 		if (ft_close_pipes(pipes, 0, 1))
 			rerror();
 	while (i--)
 	{
-			waitpid(-1, &ec, 0);
-			ft_update_error(WEXITSTATUS(ec));
+		waitpid(-1, &ec, 0);
+		ft_update_error(WEXITSTATUS(ec));
 	}
-
 	ft_free_2d_arr((void **)comands);
 	ft_free_2d_arr((void **)pipes);
 	return (0);
