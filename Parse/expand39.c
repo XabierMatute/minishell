58b20364adac14fe01571b6afa3bf7c3201f72e8
Xabier Matute Rusinol <xmatute-@c4r8s5.42urduliz.com>
2023-03-06 16:57:55 +0100

    expand39.c normalizado

--- Parse/expand39.c
+++ Parse/expand39.c
@@ -6,13 +6,13 @@
 /*   By: xmatute- <xmatute-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/10 12:13:51 by xmatute-          #+#    #+#             */
-/*   Updated: 2023/02/10 15:53:03 by xmatute-         ###   ########.fr       */
+/*   Updated: 2023/03/06 16:57:23 by xmatute-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-static size_t e39len(char *s)
+static size_t	e39len(char *s)
 {
 	size_t	l;
 
@@ -22,30 +22,20 @@ static size_t e39len(char *s)
 	return (l);
 }
 
-char *expand39(char *s)
+char	*expand39(char *s)
 {
 	char	*e;
 	size_t	l;
-	
-	// printf("s39 = %s\n", s);
-	if (!s)
-		return(0);
-	if (!*s)
-		return(ft_strdup(""));
-	l = e39len(s);
-	// printf("l39 = %i\n", l);
 
+	if (!s)
+		return (0);
+	if (!*s)
+		return (ft_strdup(""));
+	l = e39len(s);
 	e = ft_calloc(l + 1, sizeof(char));
 	if (!e)
-		return(0);//printear error de memoria
-	
+		return (merror(), NULL);
 	while (l--)
-	{
-		// printf("e[%i] = %c\n", l, s[l]);
-
 		e[l] = s[l];
-	}
-	// printf("e39 = %s\n", e);
-
 	return (ft_strjoinfree(e, expand(s + ft_strlen(e) + 1)));
 }
