26d2a767631644991d94af62e6bdf0311bfb5a38
Xabier Matute Rusinol <xmatute-@c4r8s5.42urduliz.com>
2023-03-06 17:26:37 +0100

    main.c normalizado

--- main.c
+++ main.c
@@ -6,24 +6,21 @@
 /*   By: xmatute- <xmatute-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/19 16:47:03 by xmatute-          #+#    #+#             */
-/*   Updated: 2023/02/14 13:22:45 by xmatute-         ###   ########.fr       */
+/*   Updated: 2023/03/06 17:26:16 by xmatute-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-
-int main(int argc, char **argv, char **env)
+int	main(int argc, char **argv, char **env)
 {
 	(void) argc;
 	(void) argv;
-	
 	env2 = env;
 	if (!ft_create_env(env))
 	{
 		ft_printf("⚠️Error de enviroment\n");
-		return(3);
+		return (3);
 	}
-				// printf("PID: %d\n\n", getpid());
-    return (minishell());
+	return (minishell());
 }
